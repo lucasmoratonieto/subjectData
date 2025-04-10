7654924c3350aaff7c27dc4137731637deb16a16
Lucas <lukmorato@gmail.com>
2025-04-10 19:29:05 +0200

    Verifica token en cada accion que hagan en la pagina web

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,17 @@ import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Interceptor para ENVIAR el token en cada petición ✅
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem('jwtToken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// Interceptor para manejar respuestas de error (401)
 axios.interceptors.response.use(
   response => response,
   error => {
@@ -16,13 +27,11 @@ axios.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -4,6 +4,7 @@ import jwt from 'jsonwebtoken';
 export const verifyToken = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1]; // Obtener el token
 
+
   if (!token) {
     return res.status(401).json({ error: 'Token no proporcionado' });
   }
